54c45da19028f37fca05094e5254ae5102dc6e53
zzangse1 <80884741+zzangse1@users.noreply.github.com>
2025-03-27 22:50:45 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 18.06 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.kt
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.kt
@@ -1,5 +1,5 @@
 class Solution {
     fun solution(my_string: String): Array<String> {
-        return my_string.split(" ").filter { it != "" }.toTypedArray()
+        return my_string.trim().split("\\s+".toRegex()).toTypedArray()
     }
 }
