79eb8d8a6cd07268e04bc5a07f0c201110cd3033
zzangse1 <80884741+zzangse1@users.noreply.github.com>
2025-05-18 11:07:16 +0900

    [level 1] Title: 콜라츠 추측, Time: 5.02 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.kt
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.kt
@@ -1,12 +1,12 @@
 class Solution {
     fun solution(num: Int): Int {
-        if (num == 1) return 0
-        var count = 0
         var current = num.toLong()
-        while (current != 1L && count < 500) {
-            current = if (current % 2 == 0L) current / 2 else current * 3 + 1
-            count++
-        }
-        return if (current == 1L) count else -1
+        return (0 until 500).firstOrNull {
+            if (current == 1L) true
+            else {
+                current = if (current % 2 == 0L) current / 2 else current * 3 + 1
+                false
+            }
+        } ?: -1
     }
 }
