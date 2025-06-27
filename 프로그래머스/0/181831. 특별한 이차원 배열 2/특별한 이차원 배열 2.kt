25e3ab3cdfbeda8893ec9838a8dfb79192cbb59e
zzangse1 <80884741+zzangse1@users.noreply.github.com>
2025-06-27 16:05:58 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.42 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.kt
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.kt
@@ -1,16 +1,10 @@
 class Solution {
     fun solution(arr: Array<IntArray>): Int {
-        var answer: Int = 0
-        loop@ for (i in arr.indices) {
+        for (i in arr.indices) {
             for (j in arr.indices) {
-                if (arr[i][j] == arr[j][i]) {
-                    answer = 1
-                } else {
-                    answer = 0
-                    break@loop
-                }
+                if (arr[i][j] != arr[j][i]) return 0
             }
         }
-        return answer
+        return 1
     }
 }
