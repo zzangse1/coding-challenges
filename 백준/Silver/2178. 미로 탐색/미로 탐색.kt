f9a5d0c287c467d887670fb8d6b36aab0e1baedd
zzangse1 <80884741+zzangse1@users.noreply.github.com>
2025-07-09 17:05:03 +0900

    [Silver I] Title: 미로 탐색, Time: 196 ms, Memory: 26352 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.kt
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.kt
@@ -1,25 +1,25 @@
 fun main() {
-    val dy = listOf(-1, 0, 1, 0)
-    val dx = listOf(0, 1, 0, -1)
+    val dy = arrayOf(-1, 0, 1, 0)
+    val dx = arrayOf(0, 1, 0, -1)
     val (n, m) = readln().split(" ").map { it.toInt() }
-    val miro = List(n) {
+    val matrix = Array(n) {
         readln().map { it.digitToInt() }
     }
     val visited = Array(n) { IntArray(m) }
-    val queue: ArrayDeque<Pair<Int, Int>> = ArrayDeque()
+    val queue = ArrayDeque<Pair<Int, Int>>()
     visited[0][0] = 1
     queue.add(Pair(0, 0))
 
     while (queue.isNotEmpty()) {
         val (y, x) = queue.removeFirst()
-        for (i in 0 until 4) {
+        for (i in 0..<4) {
             val ny = dy[i] + y
             val nx = dx[i] + x
-            if (ny !in 0 until n || nx !in 0 until m || miro[ny][nx] == 0) continue
+            if (ny !in 0..<n || nx !in 0..<m || matrix[ny][nx] == 0) continue
             if (visited[ny][nx] != 0) continue
             visited[ny][nx] = visited[y][x] + 1
             queue.add(Pair(ny, nx))
         }
     }
-    println("${visited[n-1][m-1]}")
+    println("${visited[n - 1][m - 1]}")
 }
