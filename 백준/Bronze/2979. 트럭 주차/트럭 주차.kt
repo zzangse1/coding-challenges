cac5cb41d3099cb7d4c5cc12d78075fb104b0e6f
zzangse1 <80884741+zzangse1@users.noreply.github.com>
2025-07-10 15:23:12 +0900

    [Bronze II] Title: 트럭 주차, Time: 112 ms, Memory: 20864 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.kt
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.kt
@@ -1,34 +1,20 @@
-class Main {
-    fun a(cost: List<Int>, car: List<Int>): Int {
-        val price = IntArray(car.max()) { 0 }
-        for (i in car.indices step 2) {
-            val start = car[i]
-            val end = car[i + 1]
-            for (j in start until end) {
-                price[j]++
-            }
-        }
-        return price.sumOf {
-            when (it) {
-                0 -> 0
-                1 -> it * cost[0]
-                2 -> it * cost[1]
-                else -> it * cost[2]
-            }
-        }
-    }
-}
-
-
 fun main() {
-    val main = Main()
-    val cost = readln().split(" ").map { it.toInt() }.toMutableList()
-    val carList = mutableListOf<Int>()
-
-
+    val arr = IntArray(104)
+    val (a, b, c) = readln().split(" ").map { it.toInt() }
     repeat(3) {
-        val input = readln().split(" ").map { it.toInt() }
-        carList.addAll(input)
+        val (s, e) = readln().split(" ").map { it.toInt() }
+        for (i in s..<e) {
+            arr[i]++
+        }
     }
-    println(main.a(cost, carList))
+
+    val result = arr.sumOf {
+        when (it) {
+            0 -> 0
+            1 -> 1 * a
+            2 -> 2 * b
+            else -> 3 * c
+        }
+    }
+    println(result)
 }
