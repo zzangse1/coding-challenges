0897e961b6b4196fcf1d0f343f7c369563ce447a
장세훈 <80884741+zzangse1@users.noreply.github.com>
2025-09-18 22:20:56 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.kt
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.kt
@@ -1,16 +1,3 @@
 class Solution {
-    fun solution(num1: Int, num2: Int): Int {
-        var answer: Int = 0
-        answer = if (num1 == num2) {
-            1
-        } else {
-            -1
-        }
-        return answer
-    }
-}
-
-fun main() {
-    val a = Solution()
-    println(a.solution(1, 1))
+    fun solution(num1: Int, num2: Int):Int = if(num1==num2) 1 else -1
 }
